93f81fb0bca7636572e8120fb919071028b678ef
omarmeneses <omar.meneses@globant.com>
2016-02-22 20:46:24 -0500

    Update Login_failed.java

--- Onboarding/src/main/java/com/maestrohealth/Onboarding/pages/onb_38_Employer_login/Login_failed.java
+++ Onboarding/src/main/java/com/maestrohealth/Onboarding/pages/onb_38_Employer_login/Login_failed.java
@@ -6,23 +6,22 @@ import org.openqa.selenium.support.FindBy;
 
 public class Login_failed {
 	
-	@FindBy(id="username")
+	@FindBy(xpath=".//*[@id='main-login-container']/div[1]/div/form/section/fieldset[1]/input")
 	private WebElement username;
 	
-	@FindBy(id="pin")
+	@FindBy(xpath=".//*[@id='main-login-container']/div[1]/div/form/section/fieldset[2]/input")
 	private WebElement password;
 	
 	
-	@FindBy(xpath="//body//div/span[4]")
+	@FindBy(xpath=".//*[@id='main-login-container']/div[1]/div/span[4]")
 	private WebElement message_response;//Message is shown when the Username and Password are not empty but they are not correct.
 	
 	//Message is shown when the Username is empty.
-	@FindBy(xpath="//body//form/div[1]/md-input-container/div[2]/div")
-	                
+	@FindBy(xpath=".//*[@id='main-login-container']/div[1]/div/form/section/fieldset[1]/div[2]/div")
 	private WebElement username_message_response;
 	
 	//Message is shown when the Password is empty.
-	@FindBy(xpath="//body//form//div[2]/md-input-container/div[2]/div")
+	@FindBy(xpath=".//*[@id='main-login-container']/div[1]/div/form/section/fieldset[2]/div[2]/div")
 	private WebElement password_message_response;
 	
 	
